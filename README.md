73f0a0a7d175477c95a3e8ab9fde88cea4c396e4
elnicomapero <75832950+elnicomapero@users.noreply.github.com>
2020-12-31 15:13:49 +0800

    Initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# My UFO
+
+This little project stems from the UFO C++ challenge in Codecademy's course.
---
+++ ufo.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+
+  greet();
+
+  std::vector<char> incorrect;
+  bool guess = false;
+
+  char letter;
+
+  while (answer != codeword && misses < 7){
+    display_misses(misses);
+    display_status(incorrect, answer);
+    std::cout << "Please enter your guess: \n";
+    std::cin >> letter;
+    std::cout << "\n\n";
+    for (int i = 0; i < codeword.size(); i++){
+      if (letter == codeword[i]){
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+    if (guess == true){
+      std::cout << "Correct!\n";
+    }
+    else {
+      incorrect.push_back(letter);
+      misses++;
+    }
+    guess = false;
+  }
+
+  end_game(answer, codeword);
+
+}
